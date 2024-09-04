288276a0c4ada51a51d7714f350c523aa691364a
livijohnson2006 <livijohnson2006@gmail.com>
2024-09-04 13:47:55 +0000

    first svelte kit test

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="index.html">ROTASMART</a></li> 
+        <li><a href="admin.html">Admin</a></li>
+        <li><a href="availability.html">Availability</a></li>
+        <li><a href="shifts.html">Shifts</a></li>
+    </ul>
+</nav>
+
+<slot/>
---
+++ src/routes/(main)/dashboard/+page.svelte
@@ -0,0 +1,8 @@
+<div class="options">
+    <ul>
+        <li><a href="upcoming.html">VIEW UPCOMING SHIFTS</a></li>
+        <li><a href="past.html">VIEW PAST SHIFTS</a></li>
+        <li><a href="availability.html">AVAILABILITY</a></li>
+        <li><a href="admin.html">ADMIN PAGE</a></li>
+    </ul>
+</div>
