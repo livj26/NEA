2d52071f279ce564dab63db3556911c0b80ac9e9
livj26 <livijohnson2006@gmail.com>
2024-09-08 23:07:56 +0100

    added new files, nav bar rendered and changes depending on page

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,10 +1,40 @@
-<nav>
-    <ul>
-        <li><a href="index.html">ROTASMART</a></li> 
-        <li><a href="admin.html">Admin</a></li>
-        <li><a href="availability.html">Availability</a></li>
-        <li><a href="shifts.html">Shifts</a></li>
-    </ul>
-</nav>
+<script context="module"> /*script is module-level. code inside script block will run once when the module is loaded, and not on every laod up*/
+    export async function load({ page }) { /*runs before the page is rendered to fetch data*/
+      return {
+        props: { // `props` allow you to pass data to a component or page*/
+          currentPage: page.path /*returns what page the user is on as a URL*/
+        }
+      };
+    }
 
-<slot/>
+  
+
+    export let currentPage; /*`export` makes `currentPage` available to other modules, allowing them to access this value.*/
+    let isLandingPage = currentPage === '/'; /*checks if page is landingpage - first page on the site*/
+
+  </script>
+  
+{#if !isLandingPage}
+    <nav>
+      <ul>
+        <li><a href="/">ROTASMART</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </nav>
+{:else}
+    <nav>
+        <ul>
+            <li><h1>ROTASMART</h1></li> 
+            <li><a href="/dashboard">Admin</a></li>
+            <li><a href="availability.html">Availability</a></li>
+            <li><a href="shifts.html">Shifts</a></li>
+        </ul>
+    </nav>
+{/if}
+
+
+  <slot/>
+  
+
+
+
