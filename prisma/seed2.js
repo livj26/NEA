2edba1465c796db1ce8136d467a207316a632e15
livj26 <livijohnson2006@gmail.com>
2024-12-05 19:03:43 +0000

    updaitng

--- prisma/seed2.js
+++ prisma/seed2.js
@@ -10,25 +10,25 @@ async function main() {
         {
             startTime: new Date("2024-11-20T09:00:00"), // October 20, 2024, 9:00 AM
             endTime: new Date("2024-11-20T17:00:00"),   // October 20, 2024, 5:00 PM
-            date: new Date("2024-10-20"),
+            date: new Date("2024-11-20"),
             employeeid: employeeId,
         },
         {
             startTime: new Date("2024-11-25T10:00:00"), // October 25, 2024, 10:00 AM
             endTime: new Date("2024-11-25T18:00:00"),   // October 25, 2024, 6:00 PM
-            date: new Date("2024-10-25"),
+            date: new Date("2024-11-25"),
             employeeid: employeeId,
         },
         {
             startTime: new Date("2024-12-22T08:00:00"), // November 5, 2024, 8:00 AM
             endTime: new Date("2024-12-22T16:00:00"),   // November 5, 2024, 4:00 PM
-            date: new Date("2024-11-05"),
+            date: new Date("2024-12-22"),
             employeeid: employeeId,
         },
         {
             startTime: new Date("2024-12-19T12:00:00"), // November 10, 2024, 12:00 PM
             endTime: new Date("2024-12-19T20:00:00"),   // November 10, 2024, 8:00 PM
-            date: new Date("2024-11-10"),
+            date: new Date("2024-12-19"),
             employeeid: employeeId,
         }
     ];
